5303adbca04971bba29137bf6993dddc639be2a0
Frederic GAURAT <fgaurat@eolem.com>
2020-12-11 10:30:30 +0100

    add DAO

--- tp3/TodoDAO.py
+++ tp3/TodoDAO.py
@@ -1,9 +1,26 @@
-
-
+import sqlite3
+from Todo import Todo
 class TodoDAO:
 
-    def __init__(self):
-        pass
+    def __init__(self,db):
+        self._conn = sqlite3.connect(db)
 
-    def save(self):
-        pass
+    def save(self,todo):
+        c = self._conn.cursor()
+        c.execute(f"INSERT INTO todos (title,dueDate,completed) VALUES ('{todo.title}',{todo.dueDate},{todo.completed})")
+        self._conn.commit()
+
+
+    def findAll(self):
+        todos = []
+        c = self._conn.cursor()
+        for row in c.execute("SELECT * FROM todos"):
+            todo = Todo(title=row[1],completed=row[3],dueDate=row[2])
+            todos.append(todo)
+
+        return todos
+
+
+    def __del__(self):
+        self._conn.close()
+
--- tp3/main.py
+++ tp3/main.py
@@ -1,13 +1,29 @@
 from Todo import Todo
+from TodoDAO import TodoDAO
+import configparser
+import argparse
 
+# python main.py conf.ini
 def main():
-    todoDAO = TodoDAO()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('conf')
+    args = parser.parse_args()
+
+    conf = configparser.ConfigParser()
+    conf.read(args.conf) # args.conf = conf.ini
+
+
+    todoDAO = TodoDAO(conf['SQLITE']['file'])
+    
     todo = Todo(title="Le titre",completed=False,dueDate=1231456)
     print(todo) # "Todo : title="Le titre",completed=False,dueDate=1231456"
 
     todoDAO.save(todo) # => 
-    todos = todoDAO.findAll()
 
+    todos = todoDAO.findAll()
+    
+    for todo in todos:
+        print(todo)
 
 if __name__ == "__main__":
     main()
