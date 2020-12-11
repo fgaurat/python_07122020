0aa3ab92fecbc34766f6127916a9004a0561fc78
Frederic GAURAT <fgaurat@eolem.com>
2020-12-11 09:50:59 +0100

    add Todo class

--- tp3/Todo.py
+++ tp3/Todo.py
@@ -1,4 +1,32 @@
 
 
 class Todo:
-    pass
+    
+    def __init__(self,title="",completed=False,dueDate=0):
+        self._title = title
+        self._completed = completed
+        self._dueDate = dueDate
+    
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self,title):
+        self._title = title
+    
+    @property
+    def completed(self):
+        return self._completed
+
+    @completed.setter
+    def completed(self,completed):
+        self._completed = completed
+    
+    @property
+    def dueDate(self):
+        return self._dueDate
+
+    @dueDate.setter
+    def dueDate(self,dueDate):
+        self._dueDate = dueDate
